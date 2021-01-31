fede5c6da4f6f4f1fffd087d488a33176e4650c5
GeoSpecs13 <36209847+GeoSpecs13@users.noreply.github.com>
2021-01-31 16:43:37 -0600

    Restructured the Repo added New functions
    Restructured repo for different functions designed for geoprocessing.

---
+++ Geoprocessing Functions/GeoProcessingFunctions.py
@@ -0,0 +1,70 @@
+'''
+@Creator = Andrew Sauerwin
+
+This library will provide a set of different geoprocessing functions to be leveraged in any workflow.
+Be sure to import the appropriate modules when using the following code.
+'''
+#
+# Imports will vary but in general, these are required for the following functions below.
+#
+import arcpy, os
+from arcpy import env
+
+def update_Values(inputs):
+'''This function reads the record of an geodatabase then changes the record of the field(s)
+ to a value of your choose.'''
+   
+	for i in inputs:
+		print("Workspace set to: {0}".format(i))
+		workspace = arcpy.env.workspace = region
+
+		# Start an edit session. Must provide the workspace.
+		edit = arcpy.da.Editor(workspace)
+		
+		# Edit session 
+		edit.startEditing(False, False)
+		
+		# Starting edit operation
+		edit.startOperation()
+
+		print("Working on records in: {0}".format(i))
+		featureclasses = arcpy.ListFeatureClasses()
+		
+		# Grabbing feature classes to work on
+		print("Starting to reset the: {0}".format(i))
+		for fc in featureclasses:
+			# Setting Spatial Reference (No longer needed but will leave for future enhancements)
+			spatial_ref = arcpy.Describe(fc).spatialReference
+			print("{0}".format(fc))
+						
+			try:
+				with arcpy.da.SearchCursor(fc, ["<Field of your choice>"]) as search_cursor:
+					for s_row in search_cursor:
+						print("\tExisting Record: {0}".format(s_row))
+				with arcpy.da.UpdateCursor(fc, ["<Field of your choice>"]) as update_cursor:
+					for a_row in update_cursor:
+						# Update record with value below
+						a_row[0] = str('<Value based on vield type, in this instance this was a Text Field>')
+						# Records get updated here
+						update_cursor.updateRow(a_row)
+				with arcpy.da.SearchCursor(fc, ["<Field of your choice>"]) as search_cursor:
+					for s_row in search_cursor:
+						print("\tChanged Record: {0}".format(s_row))
+			except Exception as error_e:
+				print("Error processing cursor: {0}".format(error_e))
+		
+		# Stop the edit operation.
+		edit.stopOperation()
+
+		# Stop the edit session and save the changes
+		edit.stopEditing(True)
+		
+		# Function process complete
+		print("Reset complete.")
+	return
+			
+# Variable for setting the database
+var_X = r""
+
+# List to pass into the function can take one or more.
+inputs = [""]
